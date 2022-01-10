0ac01103afbeb385fdfd010d6c631d0cf1c1ced7
Jibil <jibil@sayonetech.com>
2022-01-11 02:01:42 +0530

    url change

--- events_list/urls.py
+++ events_list/urls.py
@@ -23,8 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('events.urls')),
     path('', include('users.urls')),
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root':       settings.MEDIA_ROOT}),
-    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
